a7d27b96ffbc4bcbf3ed5eec5874fce0c07fe7b3
Muhsin Ay <muhsinay06@gmail.com>
2026-08-08 21:29:06 +0300

    upload inactive

--- src/app/wedding/page.tsx
+++ src/app/wedding/page.tsx
@@ -30,10 +30,13 @@ export default async function WeddingPage() {
         <main className="min-h-screen bg-neutral-950">
             <div className="max-w-7xl mx-auto pb-10">
                 <div className="flex justify-end p-6 sticky top-0 z-40 bg-linear-to-b from-neutral-950 to-transparent pointer-events-none">
-                    <div className="pointer-events-auto">
-                        {/* Uyarı: UploadModal'ın içindeki veritabanı yazma işleminin wedding tablosunu hedeflediğinden emin ol */}
+                    {
+                        /*
+                        <div className="pointer-events-auto">
                         <UploadModal />
-                    </div>
+                        </div>
+                         */
+                    }
                 </div>
 
                 <div className="space-y-8 px-4">
